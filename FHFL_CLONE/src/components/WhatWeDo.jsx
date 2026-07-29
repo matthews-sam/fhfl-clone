4a10a11ff93fc7b84c375095ab4f5922acc25d42
matthews-sam <samuelmatthews724@gmail.com>
2026-07-29 23:25:21 +0100

    completed the what we do section, created the strategy section

--- FHFL_CLONE/src/components/WhatWeDo.jsx
+++ FHFL_CLONE/src/components/WhatWeDo.jsx
@@ -1,9 +1,24 @@
 import './WhatWeDo.css'
+import what_we_do from '../assets/images/what-we-do.png';
 
 
 const WhatWeDo = () => {
     return ( 
-        <></>
+        <section className="what-we-do">
+            <div className="the-boxed-content-area">
+                <img src={what_we_do} alt="Hands holding house" className="what-we-do-img"/>
+                <div className="what-we-do-content">
+                    <h2 className="what-we-do-h2">
+                        What We Do
+                    </h2>
+                    <p className="what-we-do-p">
+                        We invest in structures to create the conditions for hard-working Nigerian families to secure their own homes.<br /><br />
+                        We do this because affordable homes not only provide a fundamental human right, providing families with the right environment to thrive and a greater overall sense of living but it is also a major driver of national growth
+                    </p>
+                    <a href='#' className="what-we-do-link">LEARN MORE<i className="fa fa-arrow-right-long what-we-do-link-i"></i></a>
+                </div>
+            </div>
+        </section>
     );
 }
  
